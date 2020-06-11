5f1a8e86910592dd42cdd1b220dfac754aabc789
Olumide <bigoluway30@gmail.com>
2020-06-11 10:56:54 +0200

    changes made

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   googleAPIKey: 'AIzaSyAz8s9GWWqnL-6u03ZY_G0U4cHPXwHTvGw'
 };
 
